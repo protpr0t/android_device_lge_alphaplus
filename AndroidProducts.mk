a90a070ba7ee40f9ee44441801fa7a6803b937c6
protpr0t <75566875+protpr0t@users.noreply.github.com>
2026-09-05 03:42:50 +0700

    Update lunch choices for alpha_alphaplus

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,5 +8,5 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/alpha_alphaplus.mk
 
 COMMON_LUNCH_CHOICES := \
-    alphadroid_alphaplus-ap3a-userdebug \
-    alphadroid_alphaplus-ap3a-user
+    alpha_alphaplus-ap3a-userdebug \
+    alpha_alphaplus-ap3a-user
